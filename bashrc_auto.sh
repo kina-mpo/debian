c992c46e2608d0ff4027bfd36067e927d1f86a6a
kina-mpo <15107701+kina-mpo@users.noreply.github.com>
2022-01-11 10:52:39 +0100

    Update bashrc_auto.sh

--- bashrc_auto.sh
+++ bashrc_auto.sh
@@ -4,20 +4,11 @@
 # Call this script with :
 # eval "$(wget -qO- https://raw.githubusercontent.com/kina-mpo/debian/main/bashrc_auto.sh)"
 
-# Starting by getting all variables and functions
-eval "$(wget -qO- https://raw.githubusercontent.com/kina-mpo/variables/main/auto.sh)"
-eval "$(wget -qO- https://raw.githubusercontent.com/kina-mpo/functions/main/auto.sh)"
-
-# Locking the script
-lock
-
 # Deleting the ".bashrc" file into the "/etc/skel" directory :
-rm -f /etc/skel/.bashrc
+rm -rf /etc/skel
 
 # Deleting the "bash.bashrc" file into the "/etc" directory :
 rm -f /etc/bash.bashrc
 
 # Getting the new "bash.bashrc" file 
-
-# Unlocking the script
-unlock
+wget -qO /etc/bash.bashrc https://raw.githubusercontent.com/kina-mpo/debian/main/bash.bashrc
